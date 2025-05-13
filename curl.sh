1be0387acc8797aac971bc931add0554aa8b4908
cqlyj <luoyingjie0721@gmail.com>
2025-05-13 21:24:34 +0800

    feat: update authorization header in curl script for Circle API compliance screening

--- curl.sh
+++ curl.sh
@@ -1,7 +1,7 @@
 curl --request POST \
   --url https://api.circle.com/v1/w3s/compliance/screening/addresses \
   --header 'Content-Type: application/json' \
-  --header 'authorization: Bearer TEST_API_KEY:de2f4522f0c2287a45073ccf69a2f3dc:abf4751e88bd09cd2cfd63f438fe0f9b' \
+  --header 'authorization: Bearer <KEY>' \
   --data '
 {
   "idempotencyKey": "9fee50f9-5bec-4833-bb0a-8eb37ea341fe",
